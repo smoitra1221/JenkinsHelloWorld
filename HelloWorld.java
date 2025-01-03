744ad671c4cd68351d232dfaa3a7c2263096464d
smoitra1221 <somamoitra@yahoo.com>
2025-01-03 17:16:42 -0500

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args){
-    System.out.println("...****Hello World Jenkins****....");
+    System.out.println("...****Hello World Jenkins****..");
     }
 }
